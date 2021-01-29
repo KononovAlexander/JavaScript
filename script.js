0a9ccadd3e546dba2efc0fb752eba4764b305eae
KononovAlexander <kononov8214@gmail.com>
2021-01-29 13:54:37 +0200

    rewrite code, add output to console

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money =11000;
+let income = 'Фриланс';
+let addExpenses = 'Комуналка, Кафе, Интернет, Кино';
+let deposit = true;
+let mission = 100000;
+let period = 8;
+let budgetDay = money / 30;
 
-alert('Hello World!');
+console.log('money: ' + typeof money);
+console.log('income: ' + typeof  income);
+console.log('deposit: ' + typeof deposit);
 
-console.log('Hello JavaScript!');
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' юаней');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log('budgetDay: ' + budgetDay);
+
