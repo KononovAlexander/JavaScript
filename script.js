c932a506bf91594ef0d3654b3fac91e3404c64ad
KononovAlexander <kononov8214@gmail.com>
2021-02-02 15:10:30 +0200

    add functions to project:showTypeOf, getExpensesMonth, getAccumulatedMonth, getTargetMonth, getStatusIncome again

--- script.js
+++ script.js
@@ -6,44 +6,17 @@ let period = 8;
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 100000;
-
-
-
 let expenses1 = prompt('Введите обязательную статью расходов?'); 
 let amount1 = +prompt('Во сколько это обойдется?','0');
-
 let expenses2 = prompt('Введите обязательную статью расходов?');
 let amount2 = +prompt('Во сколько это обойдется?','0');
-
-
-// let missionTime = mission / budgetMonth;
 let accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2));
 let budgetDay = accumulatedMonth / 30;
 
 
-
-
-
-
-console.log('money: ' + showTypeOf(money));
-console.log('income: ' + showTypeOf(income));
-console.log('deposit: ' + showTypeOf(deposit));
-
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' юаней');
-console.log(addExpenses.toLowerCase().split(', '));
-
-
-console.log('Бюджет на месяц: ', accumulatedMonth);
-console.log('Цель будет достигнута за ' + Math.ceil(getTargetMonth(mission, accumulatedMonth)) + ' месяцев');
-console.log('Бюджет на день: ' + Math.floor(budgetDay));
-console.log( getStatusIncome(budgetDay));
-
 function showTypeOf(a){
     return typeof a;
 }
-
 
  function getExpensesMonth(a, b){
      return a + b;
@@ -75,5 +48,19 @@ function showTypeOf(a){
         return 'Что то пошло не так!';
     }
 }
+
+console.log('money: ' + showTypeOf(money));
+console.log('income: ' + showTypeOf(income));
+console.log('deposit: ' + showTypeOf(deposit));
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' юаней');
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log('Бюджет на месяц: ', accumulatedMonth);
+console.log('Цель будет достигнута за ' + Math.ceil(getTargetMonth(mission, accumulatedMonth)) + ' месяцев');
+console.log('Бюджет на день: ' + Math.floor(budgetDay));
+console.log( getStatusIncome(budgetDay));
 
 
