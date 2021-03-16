f2a53fdfe05e7e0317b4d4f04a6a39872b744a10
KononovAlexander <kononov8214@gmail.com>
2021-03-16 11:47:36 +0200

    fixex for other curator

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -3,10 +3,12 @@ const sendForm = () => {
     const errorMessage = 'Что то пошло не так...',
           loadMessage = 'Загрузка...',
           alertMessage = 'Необходимо заполнить все поля!',
+          strMessage = 'Введите корректные данные!',
           successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
 
     const statusMessage = document.createElement('div');
     statusMessage.style.cssText = 'font-size: 2rem;';
+    statusMessage.style.cssText = 'color: #fff;';
 
     const postData = (requestBody) => {
 
@@ -27,6 +29,11 @@ const sendForm = () => {
         const inputs =  target.querySelectorAll('input');
         
         target.appendChild(statusMessage);
+
+        setInterval(() => {
+            target.appendChild(statusMessage).remove();
+        }, 3000);
+
         const formData = new FormData(target);
         let body = {};
         
@@ -37,7 +44,10 @@ const sendForm = () => {
     });
 
         if(Object.keys(body).length === inputs.length){
-            postData(body)
+
+            if(inputs[0].value.length > 2 && inputs[2].value.replace(/ /g, '').length < 12 && inputs[2].value.replace(/ /g, '').length > 7) {
+                
+                postData(body)
                 .then(statusMessage.textContent = loadMessage)
                 .then((response) => {
                     console.log('response.type: ', response.type);
@@ -45,18 +55,27 @@ const sendForm = () => {
                     if(response.status !== 200){
                         throw new Error('network status is not 200');
                     }
-                        statusMessage.textContent = successMessage;
+                    statusMessage.textContent = successMessage;
+                    
                 })
                 .then(inputs.forEach((input) =>{
                     setTimeout(() =>{
                         input.value = '';
-
+                        
                     }, 2000);
                 }))
+                .then(setTimeout(() => {
+                    document.querySelector('.popup').style.display = 'none';
+                }, 5000))
                 .catch((error) => {
                     statusMessage.textContent = errorMessage;
                     console.log(error);
                 });
+            }else{
+
+                statusMessage.textContent = strMessage;
+            }
+
         }else{
             statusMessage.textContent = alertMessage;
         }
